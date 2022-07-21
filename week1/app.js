35b41e3c17ad3138b24b4e5b3cad49b0745348fc
unknown <equiliberalium89@gmail.com>
2022-07-21 19:49:16 -0400

    .

---
+++ week1/app.js
@@ -0,0 +1,8 @@
+// connect
+var math = require ("./math");
+
+// console
+console.log (`Your addition solution = ${math.addition(2,3)}`)
+console.log (`Your subtraction solution = ${math.subtraction(6,8)}`)
+console.log (`Your multiplication solution = ${math.multiply(5,5)}`)
+console.log (`Your division solution = ${math.divide(10,2)}`)
---
+++ week1/math.js
@@ -0,0 +1,25 @@
+// =>
+const addition = (x, y) => {
+    return x + y;
+}
+
+const subtraction = (x, y) => {
+    return x - y;
+}
+
+const multiply = (x, y) => {
+    return x * y;
+}
+
+const divide = (x, y) => {
+    return x / y;
+}
+
+
+// mod exp
+module.exports = {
+    addition,
+    subtraction,
+    multiply,
+    divide
+}
