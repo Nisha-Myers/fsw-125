578a12a75d2b97318c50851e516bdc8b9dab69f4
Nisha-Myers <98876001+Nisha-Myers@users.noreply.github.com>
2022-07-22 22:31:30 -0400

    Update app.js

--- week1/app.js
+++ week1/app.js
@@ -1,8 +1,9 @@
-// connect math file
+// connecting math JavaScript file
 var math = require ("./math");
 
+
 // console.log math examples
-console.log (`Your addition solution = ${math.addition(2,3)}`)
-console.log (`Your subtraction solution = ${math.subtraction(6,8)}`)
-console.log (`Your multiplication solution = ${math.multiply(5,5)}`)
-console.log (`Your division solution = ${math.divide(10,2)}`)
+console.log (`Your addition solution = ${math.addition(30,60)}`)
+console.log (`Your subtraction solution = ${math.subtraction(30,25)}`)
+console.log (`Your multiplication solution = ${math.multiply(50,2)}`)
+console.log (`Your division solution = ${math.divide(100,10)}`)
