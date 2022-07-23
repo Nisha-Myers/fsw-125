0419ca002fed55428f6afda2512d6a89892f1a35
unknown <equiliberalium89@gmail.com>
2022-07-22 23:23:42 -0400

    .

--- week1/math/app.js
+++ week1/math/app.js
@@ -3,7 +3,7 @@ var math = require ("./math");
 
 
 // console.log math examples
-console.log (`Your addition solution = ${math.addition(30,60)}`)
-console.log (`Your subtraction solution = ${math.subtraction(30,25)}`)
-console.log (`Your multiplication solution = ${math.multiply(50,2)}`)
-console.log (`Your division solution = ${math.divide(100,10)}`)
+console.log (`Addition solution = ${math.addition(30,60)}`)
+console.log (`Subtraction solution = ${math.subtraction(30,25)}`)
+console.log (`Multiplication solution = ${math.multiply(50,2)}`)
+console.log (`Division solution = ${math.divide(100,10)}`)
