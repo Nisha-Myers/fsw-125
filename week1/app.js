5fabbd266e26ce8ff058a04f73be3dba4cf10164
Nisha-Myers <98876001+Nisha-Myers@users.noreply.github.com>
2022-07-22 22:03:12 -0400

    Update app.js

--- week1/app.js
+++ week1/app.js
@@ -1,7 +1,7 @@
-// connect
+// connect math file
 var math = require ("./math");
 
-// console
+// console.log math examples
 console.log (`Your addition solution = ${math.addition(2,3)}`)
 console.log (`Your subtraction solution = ${math.subtraction(6,8)}`)
 console.log (`Your multiplication solution = ${math.multiply(5,5)}`)
